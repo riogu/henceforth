842ac9296cddad6e3d12118269ce33bd0f66ddd5
João Novo <joaonovo05@protonmail.com>
2026-01-22 19:01:09 +0000

    generalized builder with associated types and added helper functions

--- src/hfs/builder/builder.rs
+++ src/hfs/builder/builder.rs
@@ -1,6 +1,6 @@
-use crate::{
-    hfs::Type,
-    hfs::token::{Literal, TokenKind},
+use crate::hfs::{
+    token::{Literal, TokenKind},
+    Type, UnresolvedOperation,
 };
 
 pub enum BuilderOperation {
@@ -19,24 +19,44 @@ pub enum BuilderOperation {
     LessThan,
     LessThanEq,
 }
+impl BuilderOperation {
+    pub fn to_unresolved_op(&self) -> UnresolvedOperation {
+        match self {
+            BuilderOperation::Add => UnresolvedOperation::Add,
+            BuilderOperation::Subtract => UnresolvedOperation::Sub,
+            BuilderOperation::Multiply => UnresolvedOperation::Mul,
+            BuilderOperation::Divide => UnresolvedOperation::Div,
+            BuilderOperation::Modulo => UnresolvedOperation::Mod,
+            BuilderOperation::Not => UnresolvedOperation::Not,
+            BuilderOperation::Or => UnresolvedOperation::Or,
+            BuilderOperation::And => UnresolvedOperation::And,
+            BuilderOperation::GreaterThan => UnresolvedOperation::Greater,
+            BuilderOperation::GreaterThanEq => UnresolvedOperation::GreaterEqual,
+            BuilderOperation::Equals => UnresolvedOperation::Equal,
+            BuilderOperation::NotEquals => UnresolvedOperation::NotEqual,
+            BuilderOperation::LessThan => UnresolvedOperation::Less,
+            BuilderOperation::LessThanEq => UnresolvedOperation::LessEqual,
+        }
+    }
+}
 
 pub enum PassMode {
     Copy,
     Move,
 }
 
-pub trait Builder<T> {
+pub trait Builder<T>: FunctionOps + StackOps + VariableOps + LoopOps + ControlFlowOps {
+    type Built;
     fn new() -> Self;
-    fn push(self, kind: T) -> Self;
-    fn push_many(self, kinds: Vec<T>) -> Self;
-    fn build(self) -> Vec<T>;
+    fn push(self, elem: T) -> Self;
+    fn push_many(self, elems: Vec<T>) -> Self;
+    fn build(self) -> Self::Built;
 }
 
 pub trait FunctionOps {
     fn args(self, args: Option<Vec<Type>>) -> Self;
     fn return_types(self, return_types: Option<Vec<Type>>) -> Self;
-    fn func_with(self, name: &str, args: Option<Vec<Type>>, return_type: Option<Vec<Type>>)
-    -> Self;
+    fn func_with(self, name: &str, args: Option<Vec<Type>>, return_type: Option<Vec<Type>>) -> Self;
     fn body(self) -> Self;
     fn end_body(self) -> Self;
     fn call_function(self, name: &str, mode: PassMode) -> Self;

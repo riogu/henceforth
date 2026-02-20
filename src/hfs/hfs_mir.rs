137e286b5f9822dc86a821714415cef5a065eb85
João Novo <joaonovo05@protonmail.com>
2026-02-20 12:13:09 +0000

    added colors

--- src/hfs/hfs_mir.rs
+++ src/hfs/hfs_mir.rs
@@ -3,6 +3,8 @@ use std::{
     fmt::{format, Display},
 };
 
+use colored::{ColoredString, Colorize, CustomColor};
+
 use crate::hfs::{ast::*, IrArena, Literal, SourceInfo};
 /*
 =================================================================================================
@@ -181,26 +183,26 @@ pub enum CfgOperation {
 
 // Debug printing trait and implementations
 fn get_repr_many<T: CfgPrintable>(items: &Vec<T>, arena: &IrArena, sep: &str) -> String {
-    items.iter().map(|item| item.get_repr(arena)).collect::<Vec<String>>().join(sep)
+    items.iter().map(|item| item.get_repr(arena).to_string()).collect::<Vec<String>>().join(sep)
 }
 
 pub trait CfgPrintable {
-    fn get_repr(&self, arena: &IrArena) -> String;
+    fn get_repr(&self, arena: &IrArena) -> ColoredString;
 }
 
 impl CfgPrintable for Type {
-    fn get_repr(&self, arena: &IrArena) -> String {
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
         match self {
-            Type::Int { .. } => String::from("i32"),
-            Type::String { .. } => String::from("str"),
-            Type::Bool { .. } => String::from("bool"),
-            Type::Float { .. } => String::from("f32"),
+            Type::Int { .. } => String::from("i32").yellow(),
+            Type::String { .. } => String::from("str").yellow(),
+            Type::Bool { .. } => String::from("bool").yellow(),
+            Type::Float { .. } => String::from("f32").yellow(),
             Type::Tuple { type_ids, .. } => {
                 // ID -> Type -> string representation
                 let type_reprs: Vec<String> =
-                    type_ids.iter().map(|id| arena.get_type(*id)).map(|typ| typ.get_repr(arena)).collect();
+                    type_ids.iter().map(|id| arena.get_type(*id)).map(|typ| typ.get_repr(arena).to_string()).collect();
 
-                type_reprs.join(" ")
+                type_reprs.join(" ").into()
             },
         }
     }
@@ -243,105 +245,144 @@ impl CfgFunction {
 }
 
 impl CfgPrintable for CfgFunction {
-    fn get_repr(&self, arena: &IrArena) -> String {
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
         let params = arena.get_type(self.param_type).clone();
         let returns = arena.get_type(self.return_type).clone();
-        let blocks_repr =
-            self.collect_blocks(arena).iter().map(|block| block.get_repr(arena)).collect::<Vec<String>>().join("\n");
-        format!("fn {}: ({}) -> ({}) {{\n{}\n}}", self.name, params.get_repr(arena), returns.get_repr(arena), blocks_repr)
+        let blocks_repr = self
+            .collect_blocks(arena)
+            .iter()
+            .map(|block| block.get_repr(arena).to_string())
+            .collect::<Vec<String>>()
+            .join("\n");
+        format!(
+            "{} {}{} {}{}{} {} {}{}{} {}\n{}\n{}",
+            "fn".purple(),
+            self.name.blue(),
+            ":".custom_color(CustomColor::new(129, 137, 150)),
+            "(".custom_color(CustomColor::new(129, 137, 150)),
+            params.get_repr(arena),
+            ")".custom_color(CustomColor::new(129, 137, 150)),
+            "->".bright_blue(),
+            "(".custom_color(CustomColor::new(129, 137, 150)),
+            returns.get_repr(arena),
+            ")".custom_color(CustomColor::new(129, 137, 150)),
+            "{".custom_color(CustomColor::new(129, 137, 150)),
+            blocks_repr,
+            "}".custom_color(CustomColor::new(129, 137, 150)),
+        )
+        .into()
     }
 }
 
 impl CfgPrintable for GlobalIrVarDeclaration {
-    fn get_repr(&self, arena: &IrArena) -> String {
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
         let typ = arena.get_type(self.hfs_type);
-        format!("let {}: {};", self.name, typ.get_repr(arena))
+        format!(
+            "{} {}{} {}{}",
+            "let".purple(),
+            self.name,
+            ":".custom_color(CustomColor::new(129, 137, 150)),
+            typ.get_repr(arena),
+            ";".custom_color(CustomColor::new(129, 137, 150))
+        )
+        .into()
     }
 }
 
 impl CfgPrintable for CfgOperation {
-    fn get_repr(&self, arena: &IrArena) -> String {
-        match self {
-            CfgOperation::Add(a, b) => format!("{} + {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Sub(a, b) => format!("{} - {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Mul(a, b) => format!("{} * {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Div(a, b) => format!("{} / {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Mod(a, b) => format!("{} % {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Equal(a, b) => format!("{} == {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::NotEqual(a, b) => format!("{} != {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Less(a, b) => format!("{} < {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::LessEqual(a, b) => format!("{} <= {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Greater(a, b) => format!("{} > {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::GreaterEqual(a, b) => format!("{} >= {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Or(a, b) => format!("{} || {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::And(a, b) => format!("{} && {}", arena.inst_name(*a), arena.inst_name(*b)),
-            CfgOperation::Not(a) => format!("!{}", arena.inst_name(*a)),
-        }
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
+        let op = match self {
+            CfgOperation::Add(a, b) => format!("{} {} {}", arena.inst_name(*a), "+".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Sub(a, b) => format!("{} {} {}", arena.inst_name(*a), "-".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Mul(a, b) => format!("{} {} {}", arena.inst_name(*a), "*".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Div(a, b) => format!("{} {} {}", arena.inst_name(*a), "/".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Mod(a, b) => format!("{} {} {}", arena.inst_name(*a), "%".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Equal(a, b) => format!("{} {} {}", arena.inst_name(*a), "==".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::NotEqual(a, b) => format!("{} {} {}", arena.inst_name(*a), "!=".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Less(a, b) => format!("{} {} {}", arena.inst_name(*a), "<".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::LessEqual(a, b) => format!("{} {} {}", arena.inst_name(*a), "<=".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Greater(a, b) => format!("{} {} {}", arena.inst_name(*a), ">".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::GreaterEqual(a, b) => format!("{} {} {}", arena.inst_name(*a), ">=".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Or(a, b) => format!("{} {} {}", arena.inst_name(*a), "||".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::And(a, b) => format!("{} {} {}", arena.inst_name(*a), "&&".bright_blue(), arena.inst_name(*b)),
+            CfgOperation::Not(a) => format!("{}{}", "!".bright_blue(), arena.inst_name(*a)),
+        };
+        op.into()
     }
 }
 
 impl CfgPrintable for TerminatorInst {
-    fn get_repr(&self, arena: &IrArena) -> String {
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
         match self {
-            TerminatorInst::Return(_, inst_id) => format!("return {}", arena.inst_name(*inst_id)),
+            TerminatorInst::Return(_, inst_id) => format!("{} {}", "return".purple(), arena.inst_name(*inst_id)).into(),
             TerminatorInst::Branch { cond, true_block, false_block, .. } => format!(
-                "branch {}, {}, {}",
+                "{} {}, {}, {}",
+                "branch".purple(),
                 arena.inst_name(*cond),
                 arena.get_block(*true_block).name,
                 arena.get_block(*false_block).name
-            ),
-            TerminatorInst::Jump(_, block_id) => format!("jump {}", arena.get_block(*block_id).name),
-            TerminatorInst::Unreachable => String::from("unreachable"),
+            )
+            .into(),
+            TerminatorInst::Jump(_, block_id) => format!("{} {}", "jump".purple(), arena.get_block(*block_id).name).into(),
+            TerminatorInst::Unreachable => format!("{}", "unreachable".purple()).into(),
         }
     }
 }
 
 impl CfgPrintable for Instruction {
-    fn get_repr(&self, arena: &IrArena) -> String {
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
         match self {
             Instruction::Parameter { source_info, index, type_id } =>
-                format!("{} %arg{}", arena.get_type(*type_id).get_repr(arena), index),
+                format!("{} %arg{}", arena.get_type(*type_id).get_repr(arena), index).into(),
             Instruction::FunctionCall { source_info, args, func_id, is_move, return_values } => {
                 let func = arena.get_func(*func_id);
                 let args_repr: Vec<String> = args.iter().map(|id| arena.inst_name(*id)).collect();
-                format!("call {}, {}", func.name, args_repr.join(", "))
+                format!("{} {}, {}", "call".purple(), func.name.blue(), args_repr.join(", ")).into()
             },
             Instruction::Phi { source_info, incoming } => {
                 let incoming: Vec<String> = incoming
                     .iter()
                     .map(|(block_id, inst_id)| format!("[{}: {}]", arena.get_block(*block_id).name, arena.inst_name(*inst_id)))
                     .collect();
-                format!("phi {}", incoming.join(", "))
+                format!("{} {}", "phi".purple(), incoming.join(", ")).into()
             },
             Instruction::Tuple { source_info, instructions } => {
                 let reprs: Vec<String> = instructions.iter().map(|id| arena.inst_name(*id)).collect();
-                format!("({})", reprs.join(", "))
+                format!(
+                    "{}{}{}",
+                    "(".custom_color(CustomColor::new(129, 137, 150)),
+                    reprs.join(", "),
+                    ")".custom_color(CustomColor::new(129, 137, 150))
+                )
+                .into()
             },
             Instruction::Operation(source_info, op) => op.get_repr(arena),
             Instruction::Literal(source_info, literal) => match literal {
-                Literal::Integer(lit) => lit.to_string(),
-                Literal::Float(lit) => lit.to_string(),
-                Literal::String(lit) => format!("\"{}\"", lit),
-                Literal::Bool(lit) => lit.to_string(),
+                Literal::Integer(lit) => lit.to_string().green(),
+                Literal::Float(lit) => lit.to_string().green(),
+                Literal::String(lit) => format!("\"{}\"", lit).green(),
+                Literal::Bool(lit) => lit.to_string().green(),
             },
             Instruction::LoadElement { source_info, index, tuple } =>
-                format!("load_element {}, {}", index, arena.inst_name(*tuple)),
-            Instruction::ReturnValue { source_info, type_id } => format!("retval {}", arena.get_type(*type_id).get_repr(arena)),
-            Instruction::Load { source_info, address, type_id } => format!("load {}", arena.inst_name(*address)),
+                format!("{} {}, {}", "load_element".purple(), format!("{}", index).green(), arena.inst_name(*tuple)).into(),
+            Instruction::ReturnValue { source_info, type_id } =>
+                format!("{} {}", "retval".purple(), arena.get_type(*type_id).get_repr(arena)).into(),
+            Instruction::Load { source_info, address, type_id } =>
+                format!("{} {}", "load".purple(), arena.inst_name(*address)).into(),
             Instruction::Store { source_info, address, value } =>
-                format!("store {}, {}", arena.inst_name(*value), arena.inst_name(*address)),
+                format!("{} {}, {}", "store".purple(), arena.inst_name(*value), arena.inst_name(*address)).into(),
             Instruction::StackKeyword { source_info, name, args } => {
                 let args_repr: Vec<String> = args.iter().map(|id| arena.inst_name(*id)).collect();
-                format!("keyword {}, {}", name, args_repr.join(", "))
+                format!("keyword {}, {}", name, args_repr.join(", ")).into()
             },
-            Instruction::Alloca { source_info, type_id } => format!("alloca {}", arena.get_type(*type_id)),
+            Instruction::Alloca { source_info, type_id } => format!("{} {}", "alloca".purple(), arena.get_type(*type_id)).into(),
             Instruction::GlobalAlloca(global_ir_var_id) => todo!("joao pls pls help ;_;"),
         }
     }
 }
 
 impl CfgPrintable for BasicBlock {
-    fn get_repr(&self, arena: &IrArena) -> String {
+    fn get_repr(&self, arena: &IrArena) -> ColoredString {
         let instructions =
             self.instructions.iter().map(|inst| arena.get_instruction(*inst).clone()).collect::<Vec<Instruction>>();
         let non_term_inst_repr = self
@@ -354,12 +395,12 @@ impl CfgPrintable for BasicBlock {
             Some(id) => format!("    {}", arena.get_terminator_instruction(id).get_repr(arena)),
             None => String::from("    <no terminator>"),
         };
-        let mut parts = vec![format!("  {}:", self.name)];
+        let mut parts = vec![format!("  {}:", self.name.red())];
         if !non_term_inst_repr.is_empty() {
             parts.push(non_term_inst_repr);
         }
         parts.push(terminator_repr);
-        parts.join("\n")
+        parts.join("\n").into()
     }
 }
 impl IrArena {
